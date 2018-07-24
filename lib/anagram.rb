2ec243ab4587fe2d4909b462518b0a9f7140b2c1
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-24 10:43:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   def match(word)
     # word.each.split("") {|w| 
    
-   word.map.split("") {|w| w.chars.sort += w}
+   word.each.split("") {|w| w.chars.sort += w}
    
 #     @words_hash = word.each_with_object(Hash.new []) do |w, hash|
 #   hash[w.chars.sort] += [w].flatten
